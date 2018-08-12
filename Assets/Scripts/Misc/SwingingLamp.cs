692babd2a094a6eb1533f136e9344f149488311c
kverhegg <kenneth.verheggen@ugent.be>
2018-08-12 02:29:33 +0200

    Fixing Commit ---> missing assets

---
+++ Assets/Scripts/Misc/SwingingLamp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SwingingLamp : MonoBehaviour
+{
+    public float _Angle;
+    public float _Period;
+    private float _Time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Time = _Time + Time.deltaTime;
+        float phase = -Mathf.Sin(_Time / _Period);
+        transform.localRotation = Quaternion.Euler(new Vector3(180, 0, 180 + (phase * _Angle)));
+    }
+
+
+}
---
+++ Assets/Scripts/UI/WallRandomizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallRandomizer : MonoBehaviour
+{
+
+    public Sprite[] availableSprites;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < (int)(transform.localScale.y); i++)
+        {
+            GameObject sprite = new GameObject();
+            sprite.transform.SetParent(this.transform);
+            sprite.transform.localPosition = new Vector3(0, i, -0.5f);
+            sprite.AddComponent<SpriteRenderer>().sprite = availableSprites[Random.Range(0, availableSprites.Length - 1)];
+        }
+    }
+
+}
